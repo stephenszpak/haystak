ef838a2ab2722fcc72a95803ffe65f43dd86eed1
Stephen Szpak <srszpak@gmail.com>
2025-06-25 09:09:17 -0500

    Fix frontend API base URL

--- frontend/src/pages/JobFeed.tsx
+++ frontend/src/pages/JobFeed.tsx
@@ -3,6 +3,6 @@ import JobCard from '../components/JobCard';
 import { useSSE } from '../hooks/useSSE';
 
 export default function JobFeed() {
-  const job = useSSE<any>(`/api/jobs/stream`);
+  const job = useSSE<any>(`${import.meta.env.VITE_API_URL}/api/jobs/stream`);
   return <JobCard job={job} />;
 }
