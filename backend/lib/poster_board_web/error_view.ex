fc244c67614a26f9ac38408024b2bd748a1d268b
Stephen Szpak <srszpak@gmail.com>
2025-06-25 08:28:38 -0500

    Add ErrorView for JSON error responses

---
+++ backend/lib/poster_board_web/error_view.ex
@@ -0,0 +1,17 @@
+defmodule PosterBoardWeb.ErrorView do
+  use PosterBoardWeb, :view
+
+  def render("404.json", _assigns) do
+    %{errors: %{detail: "Not Found"}}
+  end
+
+  def render("500.json", _assigns) do
+    %{errors: %{detail: "Internal Server Error"}}
+  end
+
+  # In case no render clause matches or no
+  # template is found, let's render it as 500
+  def template_not_found(_template, assigns) do
+    render("500.json", assigns)
+  end
+end
